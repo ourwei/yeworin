45a8a0f3794d3864870305b223ddea0c73e4a242
ourwei <159703824+ourwei@users.noreply.github.com>
2024-06-29 14:33:25 +0800

    index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Discounted prems & discord nitro"];
+const statusMessages = [".gg/yeorin"];
 
 
 let currentIndex = 0;
@@ -59,7 +59,7 @@ function updateStatusAndSendMessages() {
 
   client.user.setPresence({
     activities: [{ name: currentStatus, type: ActivityType.Custom}],
-    status: 'dnd',
+    status: 'Streaming',
   });
 
   
